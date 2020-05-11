6f3305c71397e5195e98ed3e75186d8fc4fd826f
John Guzmán <47038494+jaguzmanb1@users.noreply.github.com>
2020-05-11 11:21:53 -0500

    Version 0.0.6

--- lib/logstash/filters/time-lapse.rb
+++ lib/logstash/filters/time-lapse.rb
@@ -1,11 +1,12 @@
 # encoding: utf-8
 require "logstash/filters/base"
 require "logstash/namespace"
-require_relative "class_rbtree"
 # This example filter will replace the contents of the default
 # message field with whatever you specify in the configuration.
 #
 # It is only intended to be used as an example.
+
+
 class LogStash::Filters::TimeLapse < LogStash::Filters::Base
 
   # Setting the config_name here is required. This is how you
@@ -23,35 +24,50 @@ class LogStash::Filters::TimeLapse < LogStash::Filters::Base
   config :transactionid, :validate => :string, :required => true
   config :datefield, :validate => :string, :default => "@timestamp"
   config :validationField, :validate => :string
+  config :limitTime, :validate => :number, :default => 10
+
+  def cleanHash()
+    while true
+      $hash.dup.each_key do |key|
+        d = Time.now
+        if (d.to_f - $hash[key][0].to_f > limitTime )
+          $hash.delete(key)
+        end
+      end
+      sleep limitTime
+   end
+  end
 
   public
   def register
     # Add instance variables
-	$hash = {}
-
+  $init = false
+  $hash = {}
+  x = Thread.new{cleanHash()} 
   end # def register
 
   public
   def filter(event)
+    #unless @transactionid.nil? && @transactionid.empty? && @transactionid.length < 3
+    #if @transactionid != nil && @transactionid != "" && @transactionid.length > 3
 
-    if @transactionid
-  		if $hash[event.get(@transactionid)] == nil
-  			$hash[event.get(@transactionid)] = [event.get(@datefield), event.get(@validationField)]
-  			event.set("duracion", 0)
-      else
-        hash = $hash[event.get(@transactionid)]
-        firstDate = hash[0]
-        firstService = hash[1]
+    unless event.get(@transactionid).nil?
+      unless event.get(@transactionid).empty?
+        if $hash[event.get(@transactionid)] == nil
+          $hash[event.get(@transactionid)] = [event.get(@datefield)]
+          event.set("duracion", 0)
+        else
+          hash = $hash[event.get(@transactionid)]
+          firstDate = hash[0]
 
-        
-        $hash[event.get(@transactionid)] = [event.get(@datefield), firstService]
-        event.set('duracion',  event.get('@timestamp') - firstDate)
-        event.set('ServiceName',  firstService)
+          $hash[event.get(@transactionid)] = [event.get(@datefield)]
+          event.set('duracion',  event.get('@timestamp') - firstDate)
 
-        if firstService == event.get(@validationField)
-          $hash.delete(@transactionid)
+          # if firstService == event.get(@validationField)
+          #   $hash.delete(@transactionid)
+          # end
         end
-  		end
+      end
     end
     event.set("hash.size", $hash.size())
 
